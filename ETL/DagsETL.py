e94de8c937600a8f31e4e8d759ee6218d01ab9b9
GiorgiMegeneishvili <megeneishvili997@gmail.com>
2025-07-04 00:12:14 -0700

    Update DagsETL.py

--- ETL/DagsETL.py
+++ ETL/DagsETL.py
@@ -1,1 +1,23 @@
+from extraxt import extract
+from Load import load
+from transfrom import transform
+from utils.logger import get_logger
 
+
+logger = get_logger(__name__)
+
+
+def etl():
+    try:
+        customers,orders,employees,order_details,products = extract.get_data_from_sql_server()
+        transfrom1 = transform.transform_data(customers, orders, employees, order_details, products)
+        load1 = load.load_data_into_Postgresql(transfrom1)
+        logger.info("ETL has been executed successfully")
+        return load1
+    except Exception as e:
+        logger.error(f"Error running ETL: {e}")
+        return None
+
+
+if __name__ == '__main__':
+    etl()
